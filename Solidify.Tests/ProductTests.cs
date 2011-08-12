22f36909d90d00369130282065f35b68ccd8bf25
Einar Ingebrigtsen <einar@dolittle.com>
2011-08-12 15:46:38 +0200

    Added a test

---
+++ Solidify.Tests/ProductTests.cs
@@ -0,0 +1,16 @@
+using System;
+using NUnit;
+
+namespace Solidify.Tests
+{
+	[TestFixture]
+	public class ProductTests
+	{
+		[Test]
+		public void ShouldProcessProductsCorrectly()
+		{
+			
+		}
+	}
+}
+
