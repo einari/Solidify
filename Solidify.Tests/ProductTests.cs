47407527755eefbf2c61112ebdad236cda94a74b
Einar Ingebrigtsen <einar@dolittle.com>
2011-08-12 16:03:24 +0200

    Skeleton of a test up and running.    Its really skrewed.. :)

--- Solidify.Tests/ProductTests.cs
+++ Solidify.Tests/ProductTests.cs
@@ -1,5 +1,6 @@
 using System;
-using NUnit;
+using NUnit.Framework;
+using System.Xml;
 
 namespace Solidify.Tests
 {
@@ -9,7 +10,23 @@ namespace Solidify.Tests
 		[Test]
 		public void ShouldProcessProductsCorrectly()
 		{
-			
+            XmlDocument productsXmlDocument = Products.Load();
+
+            Processor.ProcessKomplettProducts(productsXmlDocument);
+            Processor.ProcessNorekProducts(productsXmlDocument);
+            Processor.ProcessItegraProducts(productsXmlDocument);
+            Processor.ProcessMpxProducts(productsXmlDocument);
+
+            XmlDocument komplettProducts = Products.Load(310);
+            int count = komplettProducts.SelectNodes[].Count;
+            Assert.AreEqual(count,42);
+
+            XmlDocument norekProducts = productsXmlDocument.Load(311);
+            count = norekProducts.SelectNodes[].Count;
+            Assert.That(count,Is.EqualTo(10));
+
+            XmlNodeList nodes = productsXmlDocument.SelectNodes("/Row/[RowType == 3]");
+            Assert.AreEqual(nodes.Count, 4);
 		}
 	}
 }
